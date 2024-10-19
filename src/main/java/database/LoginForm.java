d1351798e60ce6aeb459e8ea92f310498d19b785
Sakshi Patel <w0860745@myscc.ca>
2024-10-19 14:00:59 -0400

    Create new package database and new class LoginForm

---
+++ src/main/java/database/LoginForm.java
@@ -0,0 +1,13 @@
+package database;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginForm extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        
+    }
+}
