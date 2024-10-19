f48f57c599dd1d15b5d2f1e0d78c61c0537029d5
Sakshi Patel <w0860745@myscc.ca>
2024-10-19 15:15:38 -0400

    Add labels for username,password, * server location, database name. * Set borderpane as root pane and set up basic * scene and stage to start the application.

--- src/main/java/org/example/java3_final_project/LoginForm.java
+++ src/main/java/org/example/java3_final_project/LoginForm.java
@@ -2,9 +2,11 @@ package org.example.java3_final_project;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,10 +15,20 @@ public class LoginForm extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-
+        //Labels for username,password,server location and database name
         Label username_label = new Label("Username:");
+        Label password_label = new Label("Password:");
+        Label server_label = new Label("Server Location:");
+        Label database_label = new Label("Database Name:");
+
+        //sub layout vbox
+        VBox vBox = new VBox(username_label,password_label,server_label,database_label);
+        vBox.setAlignment(Pos.CENTER);
+
+        //root pane
         BorderPane root = new BorderPane();
-        root.setCenter(username_label);
+        root.setCenter(vBox);
+
         Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
