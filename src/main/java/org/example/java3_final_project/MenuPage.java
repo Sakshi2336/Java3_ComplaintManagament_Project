b0a1c545cb9e217a7fc3e7d22e60841b06541f7a
Giankaur03 <W0852088@myscc.ca>
2024-10-19 15:54:41 -0400

    Added the vbox and fonts to the labels.

--- src/main/java/org/example/java3_final_project/MenuPage.java
+++ src/main/java/org/example/java3_final_project/MenuPage.java
@@ -1,9 +1,12 @@
 package org.example.java3_final_project;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,14 +15,23 @@ public class MenuPage extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        System.out.println("What do you want to do?");
+        Label label = new Label("What do you want to do?");
+        label.setFont(Font.font(25));
+
         Button viewComp = new Button("View All Complaints");
+        viewComp.setOnAction(e-> System.out.println("view complaint"));
+
         Button addComp = new Button("Add New Complaints");
+        addComp.setOnAction(e-> System.out.println("add complaint"));
+
         Button viewStat = new Button("View Statistics");
+        viewStat.setOnAction(e-> System.out.println("view statistics"));
 
-        VBox organise = new VBox(viewComp, addComp, viewStat);
+        VBox organise = new VBox(15);
+        organise.getChildren().setAll(label, viewComp, addComp, viewStat);
+        organise.setAlignment(Pos.CENTER);
 
-        Scene scene = new Scene(organise, 500, 600);
+        Scene scene = new Scene(organise, 500, 500);
         stage.setTitle("Menu Page");
         stage.setScene(scene);
         stage.show();
