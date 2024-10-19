cb79f7f71377e66160fbe3769208c8ad0ffa7cfb
Giankaur03 <W0852088@myscc.ca>
2024-10-19 15:24:16 -0400

    Created the buttons for the menu page and displayed them

--- src/main/java/org/example/java3_final_project/MenuPage.java
+++ src/main/java/org/example/java3_final_project/MenuPage.java
@@ -1,4 +1,28 @@
 package org.example.java3_final_project;
 
-public class MenuPage {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuPage extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        System.out.println("What do you want to do?");
+        Button viewComp = new Button("View All Complaints");
+        Button addComp = new Button("Add New Complaints");
+        Button viewStat = new Button("View Statistics");
+
+        VBox organise = new VBox(viewComp, addComp, viewStat);
+
+        Scene scene = new Scene(organise, 500, 600);
+        stage.setTitle("Menu Page");
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
