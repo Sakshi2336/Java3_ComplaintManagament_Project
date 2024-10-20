e6ee5fdb0350408bcfe0700d7fb80c2d514de432
Giankaur03 <W0852088@myscc.ca>
2024-10-20 10:35:49 -0400

    Changed the buttons to the tab.

--- src/main/java/org/example/java3_final_project/MenuPage.java
+++ src/main/java/org/example/java3_final_project/MenuPage.java
@@ -5,6 +5,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -15,20 +17,25 @@ public class MenuPage extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
+
         Label label = new Label("What do you want to do?");
         label.setFont(Font.font(25));
 
-        Button viewComp = new Button("View All Complaints");
-        viewComp.setOnAction(e-> System.out.println("view complaint"));
+        TabPane tabpane = new TabPane();
 
-        Button addComp = new Button("Add New Complaints");
-        addComp.setOnAction(e-> System.out.println("add complaint"));
+        Tab viewComp = new Tab("View All Complaints");
+//        viewComp.setOnAction(e-> System.out.println("view complaint"));
 
-        Button viewStat = new Button("View Statistics");
-        viewStat.setOnAction(e-> System.out.println("view statistics"));
+        Tab addComp = new Tab("Add New Complaints");
+//        addComp.setOnAction(e-> System.out.println("add complaint"));
+
+        Tab viewStat = new Tab("View Statistics");
+//        viewStat.setOnAction(e-> System.out.println("view statistics"));
+
+        tabpane.getTabs().addAll(viewComp, addComp, viewStat);
 
         VBox organise = new VBox(15);
-        organise.getChildren().setAll(label, viewComp, addComp, viewStat);
+        organise.getChildren().setAll(label,tabpane);
         organise.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(organise, 500, 500);
