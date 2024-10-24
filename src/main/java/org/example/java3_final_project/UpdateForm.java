b3c19e5dbe4afb5997db7486b1af76c9a3a2e55a
Prabhjeet Singh <W0849306@myscc.ca>
2024-10-23 23:24:27 -0400

    Intializing with Update FOrm

---
+++ src/main/java/org/example/java3_final_project/UpdateForm.java
@@ -0,0 +1,16 @@
+package org.example.java3_final_project;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class UpdateForm extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("JavaFX Welcome");
+
+
+
+        primaryStage.show();
+    }
+}
