7d49a4388ad9d984e1e1cd672d154f884b49f80d
Prabhjeet Singh <W0849306@myscc.ca>
2024-10-25 00:45:59 -0400

    Creted a drop-down list *For Complaints

--- src/main/java/org/example/java3_final_project/UpdateForm.java
+++ src/main/java/org/example/java3_final_project/UpdateForm.java
@@ -2,6 +2,7 @@ package org.example.java3_final_project;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -24,9 +25,27 @@ public class UpdateForm extends Application {
         TextField tenantNameField = new TextField();
         grid.add(tenantNameField, 1, 0);
 
+        Label flatNumberLabel = new Label("Flat Number:");
+        grid.add(flatNumberLabel, 0, 1);
+        TextField flatNumberField = new TextField();
+        grid.add(flatNumberField, 1, 1);
+
+        Label contactInfoLabel = new Label("Contact Information:");
+        grid.add(contactInfoLabel, 0, 2);
+        TextField contactInfoField = new TextField();
+        grid.add(contactInfoField, 1, 2);
+
+        Label complaintCategoryLabel = new Label("Complaint Category:");
+        grid.add(complaintCategoryLabel, 0, 3);
+        ComboBox<String> complaintCategoryCombo = new ComboBox<>();
+        complaintCategoryCombo.getItems().addAll("Plumbing", "Electrical", "Pest Control", "Heating", "other");
+        grid.add(complaintCategoryCombo, 1, 3);x
 
 
 
 
     }
-}
+
+
+    }
+
