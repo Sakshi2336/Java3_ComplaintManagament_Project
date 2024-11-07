be92dcc7df5dbab05c3446c214dacf616ad76522
Sakshi Patel <w0860745@myscc.ca>
2024-11-07 00:48:34 -0500

    Add createTable method in Database class and call it into try block

--- src/main/java/org/example/java3_final_project/database/Database.java
+++ src/main/java/org/example/java3_final_project/database/Database.java
@@ -2,8 +2,7 @@ package org.example.java3_final_project.database;
 
 import org.example.java3_final_project.CredentialReader;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class Database {
 
@@ -20,6 +19,13 @@ public class Database {
                             CredentialReader.getDbUser(),
                             CredentialReader.getDbPass());
             System.out.println("Created connection!");
+            createTable(DBConst.TABLE_CATEGORY, DBConst.CREATE_TABLE_CATEGORY, connection);
+            createTable(DBConst.TABLE_FLAT, DBConst.CREATE_TABLE_FLAT, connection);
+            createTable(DBConst.TABLE_USER_TYPE, DBConst.CREATE_TABLE_USER_TYPE, connection);
+            createTable(DBConst.TABLE_USER, DBConst.CREATE_TABLE_USER, connection);
+            createTable(DBConst.TABLE_FLAT_USER, DBConst.CREATE_TABLE_FLAT_USER, connection);
+            createTable(DBConst.TABLE_COMPLAINT, DBConst.CREATE_TABLE_COMPLAINT, connection);
+            createTable(DBConst.TABLE_COMPLAINT_CATEGORY, DBConst.CREATE_TABLE_COMPLAINT_CATEGORY, connection);
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -30,6 +36,20 @@ public class Database {
             instance = new Database(); //here we can create bcz private has class access
         }
         return instance;
+    }
+
+    public void createTable(String tableName, String tableQuery, Connection connection) throws SQLException {
+        Statement createTable;
+        DatabaseMetaData md = connection.getMetaData();
+        ResultSet resultSet = md.getTables("spateljava", null, tableName, null);
+        if(resultSet.next()){
+            System.out.println(tableName + " table already exists");
+        }
+        else{
+            createTable = connection.createStatement();
+            createTable.execute(tableQuery);
+            System.out.println("The " + tableName + " table has been created");
+        }
     }
 
     //getConnection method so that I can access connection in login form and update variables
