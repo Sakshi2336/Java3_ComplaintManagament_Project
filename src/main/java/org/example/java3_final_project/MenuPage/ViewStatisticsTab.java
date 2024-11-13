ad2106ce5378fb978c0bf1f36b839952848f1741
Prabhjeet Singh <W0849306@myscc.ca>
2024-11-12 22:19:21 -0500

    Created refresh button

--- src/main/java/org/example/java3_final_project/MenuPage/ViewStatisticsTab.java
+++ src/main/java/org/example/java3_final_project/MenuPage/ViewStatisticsTab.java
@@ -1,6 +1,7 @@
 package org.example.java3_final_project.MenuPage;
 
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
@@ -13,6 +14,14 @@ public class ViewStatisticsTab extends Tab {
         this.setText("View Statistics");
         Text text = new Text("View Tab is working!");
         BorderPane root = new BorderPane();
+        chart = new PieChart();
+        chart.setTitle("Stats for Complaints");
+        Button refresh  =new Button("Refresh");
+
+
+
+
+
         root.setCenter(text);
         this.setContent(root);
     }
