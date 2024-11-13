2566fdb6558def8563e09a43f626e5a8d51d164e
Prabhjeet Singh <W0849306@myscc.ca>
2024-11-12 22:09:27 -0500

    Started working on StatisticsTab

--- src/main/java/org/example/java3_final_project/MenuPage/ViewStatisticsTab.java
+++ src/main/java/org/example/java3_final_project/MenuPage/ViewStatisticsTab.java
@@ -1,10 +1,13 @@
 package org.example.java3_final_project.MenuPage;
 
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 
 public class ViewStatisticsTab extends Tab {
+
+    private PieChart chart;
 
     public ViewStatisticsTab(){
         this.setText("View Statistics");
