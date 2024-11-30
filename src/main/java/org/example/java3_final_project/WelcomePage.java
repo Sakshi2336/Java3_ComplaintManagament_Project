a7d38f68a38fb840abcbcd8e4280b506997d95b8
Sakshi Patel <w0860745@myscc.ca>
2024-11-30 12:05:58 -0500

    Adding javaDoc and other comments.

--- src/main/java/org/example/java3_final_project/WelcomePage.java
+++ src/main/java/org/example/java3_final_project/WelcomePage.java
@@ -20,28 +20,32 @@ import org.example.java3_final_project.Tabs.MenuPage;
 import java.io.IOException;
 import java.util.Objects;
 
+
+/**
+ *  welcome_scene -> welcome page which will show basic information for
+ *  application and have button that will navigate to the main menu page.
+ *  This welcome page has:
+ *  - text objects -> for welcome , description and end text
+ *  - start button -> to navigate to the main menu page
+ *  - scale transition for start button
+ *  - Background image
+ *  - title_vBox -> to pull all 3 text objects
+ *  - button_hBox -> to put start button
+ *  - welcome_scene_rootPane -> Borderpane as main root pane
+ */
+
 public class WelcomePage extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
 
 
-        /**
-         *  welcome_scene -> welcome page which will show basic information for
-         *  our application and have button that will navigate to the main menu page.
-         *  This welcome page has:
-         *  - text objects -> for welcome , description and end text
-         *  - start button -> to navigate to the main menu page
-         *  - scale transition for start button
-         *  - title_vBox -> to pull all 3 text objects
-         *  - button_hBox -> to put start button
-         *  - welcome_scene_rootPane -> Borderpane as main root pane
-         */
-
+        //Welcome text for application at the top and css class for this text is - welcome_text
         Text welcome_text = new Text("Welcome to the Apartment Complaint Management System");
         welcome_text.getStyleClass().add("welcome_text");
 
 
+        //Description text which will provide some basic information about application and css class for this text is = description_text
         Text description_text = new Text("Welcome to the Apartment Complaint Management System (ACMS), \n" +
                 "\tyour essential tool for efficiently handling tenant complaints.\n" +
                 "Designed specifically for apartment managers, this application streamlines\n" +
@@ -50,12 +54,13 @@ public class WelcomePage extends Application {
         description_text.getStyleClass().add("description_text");
 
 
-
-
+        //End text and css class for this text is - ent_text
         Text end_text = new Text("Join us in transforming the way you manage apartment complaints.\n" +
                 "\tYour first step towards streamlined operations begin here!");
-        end_text.getStyleClass().add("end-text");
+        end_text.getStyleClass().add("end_text");
 
+
+        //Start button that will go to the main MenuPage
         Button start_button = new Button("Let's Get Started!");
         start_button.setOnAction(e->{
             MenuPage menuPage = new MenuPage();
@@ -67,16 +72,22 @@ public class WelcomePage extends Application {
             }
         });
 
+
+        //Scale transition for start button
         ScaleTransition scaleTransition = new ScaleTransition(Duration.seconds(5), start_button);
-        scaleTransition.setFromX(0.5);
-        scaleTransition.setFromY(0.5);
-        scaleTransition.setToX(1);
-        scaleTransition.setToY(1);
+        scaleTransition.setFromX(0.5);  //50% half from it is original width
+        scaleTransition.setFromY(0.5); //50% half from it is original height
+        scaleTransition.setToX(1); //to full 100% size
+        scaleTransition.setToY(1); //to full 100% size
         scaleTransition.setCycleCount(1);
         scaleTransition.play();
 
         //Image to put in background
         Image background_image_class = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/org/example/java3_final_project/Images/b3.jpg")));
+        // Define the size of the area that a BackgroundImage should fill relative
+        // to the Region it is styling. There are several properties who's values
+        // take precedence over the others. In particular there are 4 key properties,
+        // width, height, contain, and cover.
         BackgroundSize backgroundSize = new BackgroundSize(1500, 750, true, true, false, true);
         BackgroundImage backgroundImage = new BackgroundImage(background_image_class,
                 BackgroundRepeat.NO_REPEAT,
@@ -84,14 +95,16 @@ public class WelcomePage extends Application {
                 BackgroundPosition.CENTER,
                 backgroundSize);
 
-        //sub layout
+        //Sub layout:
+        // -VBox that has 3 text objects
+        // -HBox that has start button
         VBox title_vbox = new VBox(40,welcome_text,description_text,end_text);
         title_vbox.setAlignment(Pos.CENTER);
         HBox button_hbox = new HBox(start_button);
         button_hbox.setAlignment(Pos.CENTER);
         button_hbox.setPadding(new Insets(50));
 
-        //root pane
+        //Root pane - BorderPane
         BorderPane welcome_scene_rootPane = new BorderPane();
         welcome_scene_rootPane.setBackground(new Background(backgroundImage));
         welcome_scene_rootPane.getStyleClass().add("welcome_page_root");
