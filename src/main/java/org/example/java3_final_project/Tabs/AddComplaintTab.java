b539495f8e780a56e2621e1e7b752014494ff09b
Sakshi Patel <w0860745@myscc.ca>
2024-11-30 16:35:02 -0500

    CSS for gridpane and combo box.

--- src/main/java/org/example/java3_final_project/Tabs/AddComplaintTab.java
+++ src/main/java/org/example/java3_final_project/Tabs/AddComplaintTab.java
@@ -12,28 +12,43 @@ import javafx.scene.text.Text;
 import org.example.java3_final_project.pojo.*;
 import org.example.java3_final_project.tables.*;
 
-import java.util.Objects;
 
-import static javafx.collections.FXCollections.observableArrayList;
+/**
+ * The AddComplaintTab class represents a tab within the application that allows users to
+ * add a new complaint. This tab contains various fields where users can input tenant information,
+ * complaint details, and assign a manager to the complaint. It includes a form where users can select
+ * a tenant, flat number, complaint category, and manager, as well as provide a description for the complaint.
+ * The form is submitted by clicking a submit button, which saves the complaint and resets the form.
+ *
+ * <p>This tab also displays a success message when the complaint is successfully added.</p>
+ */
 
 public class AddComplaintTab extends Tab {
 
+
+    //class member ComboBox-> Make it class member so that i can use it into remove tab to refresh tha tenant information
     public static ComboBox<User> tenant_Combo = new ComboBox<>();
+
+    //public constructor
     public AddComplaintTab(){
-
-
         //Tab text
         this.setText("Add complaint");
 
 
-        //Root panes
+        //Root pane - BorderPane
         BorderPane root = new BorderPane();
+        root.getStyleClass().add("addCompRoot");
 
         GridPane tenantPane = new GridPane();
+        tenantPane.setHgap(10);
+        tenantPane.setVgap(3);
         tenantPane.getStyleClass().add("tenant-section");
 
+
+        //Text object to show message when complaint successfully added into database
         Text messageText = new Text();
 
+        //All tables
         UserTable userTable = new UserTable();
         ComplaintTable complaintTable = new ComplaintTable();
         FlatTable flatTable = new FlatTable();
@@ -41,70 +56,71 @@ public class AddComplaintTab extends Tab {
 
         //First tenant information heading
         Text tenantInfoHeading = new Text("Tenant Info");
-        tenantInfoHeading.setFont(Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR,30));
-        tenantPane.add(tenantInfoHeading,0,2);
+        tenantPane.add(tenantInfoHeading,10,1); //colum,row,col span,row span
 
 
         //Tenant name label and text field
-        //Now here I need to check if user entered tenant name is valid or not
         Label tenantName = new Label("Tenant Name:");
-        tenantPane.add(tenantName, 10, 4);
+        tenantPane.add(tenantName, 10, 7);
         tenant_Combo = new ComboBox<>();
         tenant_Combo.setItems(FXCollections.observableArrayList(userTable.getAllUser()));
-        tenantPane.add(tenant_Combo, 13, 4);
+        tenantPane.add(tenant_Combo, 17, 7);
 
         //Flat number label and combo box where calling getAllFlat method
         Label flatNumber = new Label("Flat Number:");
         ComboBox<Flat> comboFlat = new ComboBox<>();
         comboFlat.setItems(FXCollections.observableArrayList(flatTable.getAllFlat()));
         comboFlat.getSelectionModel().select(0);
-        tenantPane.add(flatNumber, 10, 5);
-        tenantPane.add(comboFlat, 13, 5);
+        tenantPane.add(flatNumber, 10, 13);
+        tenantPane.add(comboFlat, 17, 13);
 
         //sub grid pane for complaint information
         GridPane complaintPane = new GridPane();
+        complaintPane.getStyleClass().add("complaint-section");
+        complaintPane.setHgap(10);
+        complaintPane.setVgap(3);
 
         //Second heading complaint information
         Text complaintDetailHeading = new Text("Complaint Details");
         complaintDetailHeading.setFont(Font.font("Arial", FontWeight.BOLD, FontPosture.REGULAR,30));
-        complaintPane.add(complaintDetailHeading, 0, 0);
+        complaintPane.add(complaintDetailHeading, 10, 1);
 
         //Category label and combo box
         Label complaintCategory = new Label("Complaint Category:");
-        complaintPane.add(complaintCategory, 0, 1);
+        complaintPane.add(complaintCategory, 10, 7);
         CategoryTable categoryTable = new CategoryTable();
         ComboBox<Category> comboCategory = new ComboBox<>();
         comboCategory.setItems(FXCollections.observableArrayList(categoryTable.getAllCategory()));
         comboCategory.getSelectionModel().select(0);
-        complaintPane.add(comboCategory, 1, 1);
+        complaintPane.add(comboCategory, 17, 7);
 
 
         //Complaint description
         Label description = new Label("Description:");
-        complaintPane.add(description, 0, 2);
+        complaintPane.add(description, 10, 13);
         TextArea descriptionText = new TextArea();
         descriptionText.setWrapText(true);
         descriptionText.setPrefWidth(250);
         descriptionText.setPrefHeight(80);
-        complaintPane.add(descriptionText, 1, 2);
+        complaintPane.add(descriptionText, 17, 13);
 
 
         //Status label and combo box
         Label status = new Label("Status:");
-        complaintPane.add(status, 0, 3);
+        complaintPane.add(status, 25, 7);
         ComboBox<String> statusComboBox = new ComboBox<>();
         statusComboBox.getItems().addAll("Open","in Process");
         statusComboBox.setValue("Open");
-        complaintPane.add(statusComboBox, 1, 3);
+        complaintPane.add(statusComboBox, 32, 7);
 
 
         //Assigned manager label and combo box
         Label assignedManager = new Label("Assigned Manager:");
-        complaintPane.add(assignedManager, 0, 4);
+        complaintPane.add(assignedManager, 25, 13);
         ComboBox<User> managerComboBox = new ComboBox<>();
         managerComboBox.setItems(FXCollections.observableArrayList(userTable.getAllManager()));
         managerComboBox.getSelectionModel().select(0);
-        complaintPane.add(managerComboBox, 1, 4);
+        complaintPane.add(managerComboBox, 32, 13);
 
 
 
