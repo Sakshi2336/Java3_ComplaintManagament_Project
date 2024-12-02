6f3a4b840563fd5756df9f74d044fe05b414f543
Sakshi Patel <w0860745@myscc.ca>
2024-12-02 00:15:25 -0500

    Adding comments into TenantInfo class.

--- src/main/java/org/example/java3_final_project/Tabs/TenantInfo.java
+++ src/main/java/org/example/java3_final_project/Tabs/TenantInfo.java
@@ -15,23 +15,41 @@ import org.example.java3_final_project.tables.UserTable;
 
 import static org.example.java3_final_project.Tabs.AddComplaintTab.tenant_Combo;
 
+
+/**
+ * The TenantInfo class shows tenant information.
+ * It allows the manager to view, remove, and refresh the list of tenants.
+ */
+
 public class TenantInfo extends Tab {
 
+
+    //class members
     private TableView tableView;
     private UserTable userTable;
     private FlatUserTable flatUserTable;
 
     public TenantInfo() {
 
+        //tab label
         this.setText("Tenant Information");
 
+        //All needed tables
         userTable = new UserTable();
         flatUserTable = new FlatUserTable();
+
+        //Root pane BorderPane
         BorderPane root = new BorderPane();
+
+        //Sub layout Hbox
         HBox hbox = new HBox();
+
+        //Text object for showing user message and font size for that text object
         Text messageText = new Text();
         messageText.setFont(Font.font(20));
 
+
+        //TableView for showing tenant information and all 3 columns for flat num, first name and last name
         tableView = new TableView();
 
         // Tenant First Name
@@ -63,14 +81,23 @@ public class TenantInfo extends Tab {
         // Remove Tenant Button
         Button removeTenantButton = new Button("Remove Tenant");
         removeTenantButton.setOnAction(e -> {
+            //converting selected item to DisplayTenant
             DisplayTenant selectedTenant = (DisplayTenant) tableView.getSelectionModel().getSelectedItem();
-            if(selectedTenant == null){
+            if(selectedTenant == null){ //if user do not select anything then shoeing message
                 messageText.setText("Please first select tenant.");
             }else {
+                //deleting user first from FlatUser table
                 userTable.deleteTenantFromFlatUser(selectedTenant.getId());
+                //Then deleting from Users class
                 userTable.deleteTenantFromUsers(selectedTenant.getId());
+                //showing message to user
                 messageText.setText("Tenant Removed Successfully");
+                //After deleting user refreshing table view
                 refreshTable();
+                //Then updating tenant combo box in add complaint table so
+                // when manager delete user from table it will also get removed
+                // from add complaint tenant combo box which is displaying
+                // all users living in apartment
                 updateTenantComboBox(tenant_Combo, userTable);
             }
         });
@@ -90,12 +117,21 @@ public class TenantInfo extends Tab {
     }
 
 
-    // Method to refresh table content
+    /**
+     * Refresh the tenant table by clearing its current items and adding
+     * the updated items in the table.
+     */
     public void refreshTable() {
         tableView.getItems().clear();
         tableView.getItems().addAll(userTable.getPrettyTenants());
     }
 
+    /**
+     * This method update the tenant combo box which is showing users living in
+     * the apartment.
+     * @param tenant_Combo -> combo box of tenants
+     * @param userTable -> User Table for calling getAllUser method 
+     */
     public void updateTenantComboBox(ComboBox<User> tenant_Combo, UserTable userTable){
         tenant_Combo.setItems(FXCollections.observableArrayList(userTable.getAllUser()));
     }
