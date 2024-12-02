8c15d6c52bdddb5b85f1395a3db2c36a8122dcbb
Sakshi Patel <w0860745@myscc.ca>
2024-12-02 01:20:22 -0500

    Added comments in Database class.

--- src/main/java/org/example/java3_final_project/database/Database.java
+++ src/main/java/org/example/java3_final_project/database/Database.java
@@ -2,11 +2,25 @@ package org.example.java3_final_project.database;
 
 import java.sql.*;
 
+/**
+ * The Database class is for connecting to the database
+ * This class has private constructor which is for singleton pattern
+ * This class is creating tables if there are not already exist.
+ */
 public class Database {
 
-    private static Database instance;
-    private Connection connection = null; //I have not created getters yet that is why it is showing that it is never used
 
+    //Instance of the database class
+    private static Database instance;
+
+    //connection of the database
+    private Connection connection = null;
+
+    /**
+     * Private constructor for singleton pattern.
+     * It reads the database credentials from the file, establishes the database connection,
+     * and make sure that necessary tables are created in the database.
+     */
     private Database(){
         try{
             CredentialReader.readCredential();
@@ -29,6 +43,12 @@ public class Database {
         }
     }
 
+
+    /**
+     * Get the instance of the Database class if it is already exists
+     * and if it is null then create one
+     * @return either existing Database instance or new created instance
+     */
     public static Database getInstance(){
         if(instance == null){
             instance = new Database(); //here we can create bcz private has class access
@@ -36,9 +56,20 @@ public class Database {
         return instance;
     }
 
+    /**
+     * Creates a table in the database if it doesn't already exist.
+     * This method checks if a table with the specified name exists using the database metadata.
+     * If the table does not exist, it executes the provided SQL query to create the table.
+     * @param tableName the name of the table for create and check
+     * @param tableQuery sql query to create table
+     * @param connection connection to the database
+     * @throws SQLException sql error when creating or checking for table
+     */
     public void createTable(String tableName, String tableQuery, Connection connection) throws SQLException {
         Statement createTable;
         DatabaseMetaData md = connection.getMetaData();
+
+        //Check if table exists or not
         ResultSet resultSet = md.getTables(CredentialReader.getDbName(), null, tableName, null);
         if(resultSet.next()){
             System.out.println(tableName + " table already exists");
@@ -50,7 +81,10 @@ public class Database {
         }
     }
 
-    //getConnection method so that I can access connection in login form and update variables
+    /**
+     * get the connection to the database
+     * @return connection to the database
+     */
     public Connection getConnection(){
         return connection;
     }
